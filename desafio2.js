d44e6fc9e6f4ea418feae9bd65d52ef9999ed62c
Vic <victorialau.dg@gmail.com>
2023-04-02 02:01:08 -0300

    ajustes

--- desafio2.js
+++ desafio2.js
@@ -23,7 +23,7 @@ class ProductManager {
             return readContent
         }else{
             console.log('El archivo no existe');
-           // return this.#products
+            return this.#products
         }
         
     }
@@ -38,7 +38,6 @@ class ProductManager {
     //Buscar un producto por su id
     getProductById = (id) => {
         //let existingProduct = this.#products.find(product => product.id === id) VERSIÓN DEL DESAFÍO ANTERIOR
-        let contentById = JSON.parse(readContent)
         let existingProduct = this.#products.find(product => product.id === id)
 
         if (!existingProduct) throw new Error(`NOT FOUND: No existe ningún producto con el id ingresado: ${id}`);
@@ -58,16 +57,14 @@ class ProductManager {
         let id = this.#generateID()
 
         let newProduct = {
-        id,
-        title,
-        description,
-        price,
-        thumbnail,
-        code,
-        stock
+            id,
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock
         }
-
-        
 
         //Valida que todos los argumentos sean obligatorios
         if(!title || !description || !price || !thumbnail || !code || !stock){
@@ -85,17 +82,33 @@ class ProductManager {
     }
 
     
+
+    deleteProduct(id) {
+        const deleteById = this.#products.find(product => product.id === id)
+        console.log('deleteById: ', deleteById.id)
+        console.log('Borrar: ', deleteById)
+
+        if (deleteById !== -1) {
+            this.#products.splice(deleteById, 1);
+            fs.writeFileSync(filename, JSON.stringify(this.#products, null, '\t'))
+       }
+    }
+
 }
 
 const productManager = new ProductManager('./productsList.json')
 
-productManager.addProduct('Colombia', 'Auténtico café tostado colombiano', 2680,'images/cafe_barista_colombia.png', 'code125', 50) //Pushea OK
+productManager.addProduct('Colombia', 'Auténtico café tostado colombiano', 2680,'images/cafe_barista_colombia.png', 'code125', 50) //Pushea OK 
 productManager.addProduct('Mundial', 'Un café que combina las mejores notas en un blend mundial', 3500,'images/cafe_barista_mundial.png', 'code126', 30) //Pushea OK
 productManager.addProduct('Brasil', 'Todos los aromas brasileros en un mismo sabor', 3000,'images/cafe_barista_brasil.png', 'code125', 40) //No se pushea por código repetido
 productManager.addProduct('Catuai', 'Una molienda brasilera, orgánica y saludable', 4000,'images/cafe_barista_catuai.png') //No se pushea por campos incompletos
-productManager.addProduct('Mundial2', 'Un café de test', 5500,'images/cafe_barista_mundial.png', 'code127', 35) //test
+productManager.addProduct('Mundial2', 'Un café de test', 5500,'images/cafe_barista_mundial.png', 'code127', 35) //test update
+productManager.addProduct('Costa Rica', 'Originario del Valle Occidental, orgánico y de aires costarricenses', 7000,'images/cafe_barista_costarica.png', 'code128', 30) //Pushea OK
+
 
 productManager.updateProduct(3,'Un Café Customizado')
+productManager.deleteProduct(1); //borra el 1er objeto del array
+
 console.log(productManager.getProducts())
 
 
@@ -106,8 +119,9 @@ catch (error) {
     console.error(error.message);
 }
 try {
-    productManager.getProductById(2) //ID encontrado, se consologuea el resultado como objeto
+    productManager.getProductById(2) //ID encontrado, devuelve el resultado como objeto
 }
 catch (error) {
     console.error(error.message);
 }
+
